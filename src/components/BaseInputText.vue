60e6275c86b0a7f1c0f0ad72423e5783daca0f5e
KarlAnastas <karlovaanastasia9@gmail.com>
2019-07-02 11:08:51 +0900

    first commit

---
+++ src/components/BaseInputText.vue
@@ -0,0 +1,35 @@
+<template>
+  <input type="text" class="input" :value="value" v-on="listeners">
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    }
+  },
+  computed: {
+    listeners () {
+      return {
+        //pass all component listeners directly to input
+        ...this.$listeners,
+        // Override input listener to word with v-model
+        input: event => this.$emit('input', event.target.value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../variables.scss';
+
+.input {
+  border: 1px solid $vue-blue;
+  padding: 8px 10px;
+  width: 100%;
+}
+</style>
+
